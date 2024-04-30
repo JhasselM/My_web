405c8b59fa76c058a59819268c0b31ce0f16b5ef
Jhassel <154636505+JhasselM@users.noreply.github.com>
2024-04-29 23:11:54 -0600

    Update views.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Project
 
 # Create your views here.
+
 def portfolio(request):
-         projects = Project.objects.all()
-         return render(request, 'portfolio/portfolio.html', {'projects':projects})
+    projects = Project.objects.all()
+    return render(request, "portfolio/portfolio.html", {'projects': projects})
