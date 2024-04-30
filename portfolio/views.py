c83bb4cddf536b70a7660e640b0ccc6a2c53b260
Jhassel Lopez <jhass1404@gmail.com>
2024-04-29 23:34:13 -0600

    Se actualizaron las imagenes

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+'''from django.shortcuts import render
 from .models import Project
 
 # Create your views here.
 def portfolio(request):
          projects = Project.objects.all()
-         return render(request, 'portfolio/portfolio.html', {'projects':projects})
+         return render(request, 'portfolio/portfolio.html', {'projects':projects})'''
+
+from django.shortcuts import render
+from .models import Project
+
+# Create your views here.
+
+
+def portfolio(request):
+    projects = Project.objects.all()
+    return render(request, "portfolio/portfolio.html", {'projects': projects})
